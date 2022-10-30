040e4e8adcd5ea510414a28a07f6b7d7490f5a1b
Lucky2245 <95869708+Lucky2245@users.noreply.github.com>
2022-10-30 14:25:16 -0400

    Update worm.py

--- worm.py
+++ worm.py
@@ -1,3 +1,8 @@
-file_code = [""]
-vbs = open("love.vbs", "w")
-vbs.write(file_code)
+import os
+if os.path.exists("love.vbs"):
+  print("Open the file love.vbs")
+else:
+  file_code = ""
+  vbs = open("love.vbs", "w")
+  vbs.write(file_code)
+  print("Open the file love.vbs")
