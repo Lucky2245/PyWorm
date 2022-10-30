3f7dff2e1378385fc00582c3a8db6f48094ba872
Lucky2245 <95869708+Lucky2245@users.noreply.github.com>
2022-10-30 14:47:40 -0400

    Update worm.py

--- worm.py
+++ worm.py
@@ -1,8 +1,9 @@
 import os
-if os.path.exists("love.bat"):
-  print("Open the file love.bat")
+import subprocess
+if os.path.exists("file.bat"):
+  print("Run the batch file: file.bat")
 else:
-  file_code = ""
-  bat = open("love.bat", "w")
+  file_code = "@echo off echo 'Your computer has been infected. Deleting the virus will result in your computer being destroyed. If you reset your computer your computer will be destroyed!' "
+  bat = open("file.bat", "w")
   bat.write(file_code)
-  print("Open the file love.bat")
+  print("Open the file: file.bat")
