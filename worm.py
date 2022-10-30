f605f154a8a08e42d18c5c68684c8d48008e6ff0
Lucky2245 <95869708+Lucky2245@users.noreply.github.com>
2022-10-30 14:31:50 -0400

    Update worm.py

--- worm.py
+++ worm.py
@@ -3,6 +3,6 @@ if os.path.exists("love.bat"):
   print("Open the file love.bat")
 else:
   file_code = ""
-  vbs = open("love.bat", "w")
-  vbs.write(file_code)
+  bat = open("love.bat", "w")
+  bat.write(file_code)
   print("Open the file love.bat")
