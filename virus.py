e30f3f1ea1f3fcf74db7f2c66446c25d0ea82984
Lucky2245 <95869708+Lucky2245@users.noreply.github.com>
2022-10-30 14:55:51 -0400

    Update virus.py

--- virus.py
+++ virus.py
@@ -3,6 +3,6 @@ if os.path.exists("file.vbs"):
   print("Run the batch file: file.vbs")
 else:
   file_code = "x=msgbox('Warning: if you shutdown your computer or delete this program your computer will be destroyed!', 0, 'You Fell For It!')"
-  bat = open("file.vbs", "w")
-  bat.write(file_code)
+  vbs = open("file.vbs", "w")
+  vbs.write(file_code)
   print("Open the file: file.vbs")
